d53449372230ba43957874b1b0ca7ede4771feb9
Jacarte <javierca@kth.se>
2023-01-17 09:50:37 +0100

    Setting up nvimtree

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -2,7 +2,15 @@
 
 require('nvim-tree').setup({
   -- Allow using gx
-  disable_netrw = false,
-  hijack_netrw = false,
+  disable_netrw = true,
+  hijack_netrw = true,
   update_cwd = true,
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action= "dir_up" }
+      }
+    }
+  }
 })
