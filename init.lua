41e14a9783c833dc3960d9603d32512cc71df4c5
Javier Cabrera <javier.cabrera@sinch.com>
2025-03-27 09:52:48 +0100

    Adding auto format on close

--- init.lua
+++ init.lua
@@ -8,3 +8,13 @@ require('plugins')
 require('themes')  -- Theme at the end, to prevent overwrite by other plugins
 
 vim.cmd([[autocmd BufEnter * silent! lcd %:p:h]])
+
+-- Auto format
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end
+})
+
