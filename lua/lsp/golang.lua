f4203328aacab2ab31d3b3ba60e02bb47c88cfed
Javier Cabrera <javier.cabrera@sinch.com>
2025-10-17 16:09:06 +0200

    adding coverage reported

---
+++ lua/lsp/golang.lua
@@ -0,0 +1,44 @@
+local nvim_lsp = require('lspconfig')
+local utils = require('lsp.utils')
+local common_on_attach = utils.common_on_attach
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+capabilities.textDocument.semanticTokens = {
+  dynamicRegistration = false,
+  requests = {
+    range = true,
+    full = true,
+  },
+  tokenTypes = {
+    "namespace", "type", "class", "enum", "interface", "struct",
+    "typeParameter", "parameter", "variable", "property", "enumMember",
+    "event", "function", "method", "macro", "keyword", "modifier",
+    "comment", "string", "number", "regexp", "operator",
+  },
+  tokenModifiers = {
+    "declaration", "definition", "readonly", "static", "deprecated",
+    "abstract", "async", "modification", "documentation", "defaultLibrary"
+  },
+}
+
+vim.api.nvim_set_hl(0, "@lsp.type.property", { link = "Identifier" }) -- for struct fields
+vim.api.nvim_set_hl(0, "@lsp.type.variable", { link = "Identifier" })
+
+-- Go configuration
+nvim_lsp.gopls.setup({
+  capabilities = capabilities,
+  on_attach = common_on_attach,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = { "go.work", "go.mod", ".git"  },
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = false,
+      analyses = {
+        unusedparams = true
+      },
+    },
+  },
+})
+
