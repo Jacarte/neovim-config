Jacarte/neovim-config

commit 6b5259fcb50ab33332037c05190fd3468bcb89fb

lots of improvements

lua/lsp/golang.lua
@@ -1,4 +1,3 @@
-local nvim_lsp = require('lspconfig')
 local utils = require('lsp.utils')
 local common_on_attach = utils.common_on_attach
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -21,16 +20,16 @@ capabilities.textDocument.semanticTokens = {
   },
 }
 
-vim.api.nvim_set_hl(0, "@lsp.type.property", { link = "Identifier" }) -- for struct fields
+vim.api.nvim_set_hl(0, "@lsp.type.property", { link = "Identifier" })
 vim.api.nvim_set_hl(0, "@lsp.type.variable", { link = "Identifier" })
 
 -- Go configuration
-nvim_lsp.gopls.setup({
-  capabilities = capabilities,
-  on_attach = common_on_attach,
+vim.lsp.config.gopls = {
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = { "go.work", "go.mod", ".git"  },
+  root_markers = { "go.work", "go.mod", ".git" },
+  capabilities = capabilities,
+  on_attach = common_on_attach,
   settings = {
     gopls = {
       completeUnimported = true,
@@ -40,5 +39,5 @@ nvim_lsp.gopls.setup({
       },
     },
   },
-})
+}
 

lua/lsp/ts.lua
@@ -1,4 +1,3 @@
-local nvim_lsp = require('lspconfig')
 local utils = require('lsp.utils')
 local common_on_attach = utils.common_on_attach
 
@@ -7,31 +6,31 @@ local common_on_attach = utils.common_on_attach
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- TypeScript/JavaScript via tsserver
-nvim_lsp.tsserver.setup({
+-- TypeScript/JavaScript via ts_ls (formerly tsserver)
+vim.lsp.config.ts_ls = {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+  root_markers = { "tsconfig.json", "package.json", ".git" },
   capabilities = capabilities,
   on_attach = function(client, bufnr)
-    -- Disable tsserver formatting in favor of prettier or null-ls
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
     common_on_attach(client, bufnr)
   end,
-  root_dir = nvim_lsp.util.root_pattern("tsconfig.json", "package.json", ".git"),
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-  cmd = { "typescript-language-server", "--stdio" },
-})
+}
 
 -- Optional: ESLint LSP for linting
-nvim_lsp.eslint.setup({
+vim.lsp.config.eslint = {
+  cmd = vim.lsp.config.eslint.cmd,
+  filetypes = vim.lsp.config.eslint.filetypes,
+  root_markers = { ".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git" },
   capabilities = capabilities,
   on_attach = function(client, bufnr)
-    -- You can enable auto-fix on save
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       command = "EslintFixAll"
     })
     common_on_attach(client, bufnr)
   end,
-  root_dir = nvim_lsp.util.root_pattern(".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git"),
-})
+}
 

lua/plugins/lspconfig.lua
@@ -1,4 +1,3 @@
-local nvim_lsp = require('lspconfig')
 local utils = require('lsp.utils')
 local common_on_attach = utils.common_on_attach
 
@@ -9,14 +8,20 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 -- Enable language servers with common settings
 local servers = {"bashls", "clangd", "pyright", "jsonls", "dockerls"}
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup({
+  vim.lsp.config[lsp] = {
+    cmd = vim.lsp.config[lsp].cmd,
+    filetypes = vim.lsp.config[lsp].filetypes,
+    root_markers = vim.lsp.config[lsp].root_markers,
     on_attach = common_on_attach,
     capabilities = capabilities,
-  })
+  }
 end
 
 
-nvim_lsp["pyright"].setup({
+vim.lsp.config["pyright"] = {
+  cmd = vim.lsp.config["pyright"].cmd,
+  filetypes = vim.lsp.config["pyright"].filetypes,
+  root_markers = vim.lsp.config["pyright"].root_markers,
   capabilities = capabilities,
   on_attach = common_on_attach,
   settings={
@@ -25,7 +30,7 @@ nvim_lsp["pyright"].setup({
       },
       pyright = {}
   }
-})
+}
 
 require('lsp.rust')
 -- require('lsp.sumneko')

init.lua
@@ -7,7 +7,18 @@ require('commands')
 require('plugins')
 require('themes')  -- Theme at the end, to prevent overwrite by other plugins
 
-vim.cmd([[autocmd BufEnter * silent! lcd %:p:h]])
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
+    if buftype == '' or buftype == 'acwrite' then
+      local bufdir = vim.fn.expand('%:p:h')
+      if bufdir ~= '' and vim.fn.isdirectory(bufdir) == 1 then
+        vim.cmd('silent! lcd ' .. vim.fn.fnameescape(bufdir))
+      end
+    end
+  end
+})
 
 -- Auto format
 

lua/keymaps.lua
@@ -114,10 +114,10 @@ nnoremap("<leader>D", "<Cmd>lua vim.diagnostic.setloclist()<CR>")
 nnoremap("<leader><A-d>", "<Cmd>lua vim.diagnostic.setqflist()<CR>")
 
 -- Telescope
-nnoremap("<leader>ff", "<Cmd>Telescope find_files<CR>")
-nnoremap("<leader>fhf","<Cmd>Telescope find_files hidden=true<CR>")
+nnoremap("<leader>ff", "<Cmd>lua require('telescope.builtin').find_files({cwd = vim.fn.getcwd(-1, -1), prompt_title = vim.fn.getcwd(-1, -1)})<CR>")
+nnoremap("<leader>fhf","<Cmd>lua require('telescope.builtin').find_files({cwd = vim.fn.getcwd(-1, -1), hidden=true, prompt_title = vim.fn.getcwd(-1, -1)})<CR>")
 nnoremap("<leader>fb", "<Cmd>Telescope buffers<CR>")
-nnoremap("<leader>fg", "<Cmd>Telescope live_grep<CR>")
+nnoremap("<leader>fg", "<Cmd>lua require('telescope.builtin').live_grep({cwd = vim.fn.getcwd(-1, -1), prompt_title = vim.fn.getcwd(-1, -1)})<CR>")
 
 -- File explorer
 nnoremap("<leader>e", "<Cmd>NvimTreeToggle<CR>")  -- NvimTree

lua/lsp/rust.lua
@@ -1,74 +1,45 @@
--- Set completeopt to have a better completion experience
--- :help completeopt
--- menuone: popup even when there's only one match
--- noinsert: Do not insert text until a selection is made
--- noselect: Do not auto-select, nvim-cmp plugin will handle this for us.
 vim.o.completeopt = "menuone,noinsert,noselect"
-
--- Avoid showing extra messages when using completion
 vim.opt.shortmess = vim.opt.shortmess + "c"
 
-local fn = vim.fn
 local common_on_attach = require('lsp.utils').common_on_attach
 
-
--- Configure LSP through rust-tools.nvim plugin.
--- rust-tools will configure and enable certain LSP features for us.
--- See https://github.com/simrat39/rust-tools.nvim#configuration
-local opts = {
-  tools = {
-    runnables = {
-      use_telescope = true,
-    },
-    inlay_hints = {
-      auto = true,
-      show_parameter_hints = false,
-      parameter_hints_prefix = "",
-      other_hints_prefix = "",
-    },
-  },
-
-  -- all the opts to send to nvim-lspconfig
-  -- these override the defaults set by rust-tools.nvim
-  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+vim.g.rustaceanvim = {
   server = {
-    -- on_attach is a callback called when the language server attachs to the buffer
     on_attach = common_on_attach,
-    settings = {
-      -- to enable rust-analyzer settings visit:
-      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-      ["rust-analyzer"] = {
-        -- enable clippy on save
+    default_settings = {
+      ['rust-analyzer'] = {
         checkOnSave = {
           command = "clippy",
         },
         numThreads = 16,
         imports = {
-                granularity = {
-                    group = "module",
-                },
-                prefix = "self",
-            },
+          granularity = {
+            group = "module",
+          },
+          prefix = "self",
+        },
         cargo = {
-            buildScripts = {
-                 enable = true,
-            },
-             loadOutDirsFromCheck = true,
-            -- allFeatures = true
+          buildScripts = {
+            enable = true,
+          },
+          loadOutDirsFromCheck = true,
         },
         cachePriming = {
-            enable = true,
-            numThreads = 16
+          enable = true,
+          numThreads = 16
         },
         procMacro = {
-            enable = true
+          enable = true
         },
       },
     },
   },
+  tools = {
+    hover_actions = {
+      auto_focus = false,
+    },
+  },
 }
-
-require("rust-tools").setup(opts)
 local cmp = require("cmp")
 
 local format_sync_grp = vim.api.nvim_create_augroup("Format", {})

lua/lsp/utils.lua
@@ -9,8 +9,7 @@ cmd([[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
 
 -- This function defines the on_attach function for several languages which share the same key-bidings
 function M.common_on_attach(client, bufnr)
-  -- Set omnifunc
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
   local ft = vim.bo[bufnr].filetype
 
   -- Helper function

lua/plugins.lua
@@ -37,7 +37,11 @@ return require('packer').startup(function(use)
 
   use('nvim-lua/lsp-status.nvim')
   -- Rust tools
-  use("simrat39/rust-tools.nvim")
+  use({
+    'mrcjkb/rustaceanvim',
+    ft = { 'rust' },
+    tag = '5.15.1',
+  })
   -- LSP server
   use {
     "williamboman/mason.nvim",
@@ -158,7 +162,7 @@ return require('packer').startup(function(use)
   -- Signature help
   use "ray-x/lsp_signature.nvim"
 
-  use({'nvim-telescope/telescope-fzf-native.nvim', run ='make'})
+  use({'nvim-telescope/telescope-fzf-native.nvim', run ='cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'})
   -- Telescope
   use({
     'nvim-telescope/telescope.nvim',

commit 2fedca0dc136dfebbe041a3b98eebfb35dd94e5b

Fix: Restore lspconfig.setup() pattern to fix broken LSP navigation
Commit 6b5259f broke LSP navigation by switching from require('lspconfig')
with .setup() to vim.lsp.config declarations. The vim.lsp.config approach
only registers configs but doesn't auto-start servers without additional
autocmd logic.

This fix reverts to the proven working pattern:
- lua/lsp/golang.lua: Use nvim_lsp.gopls.setup()
- lua/lsp/ts.lua: Use nvim_lsp.tsserver.setup() and nvim_lsp.eslint.setup()
- lua/plugins/lspconfig.lua: Use nvim_lsp[server].setup() pattern

LSP servers (gopls, ts_ls) now auto-start when opening matching file types.
Verified with headless testing: both gopls and ts_ls clients connect properly.

lua/lsp/golang.lua
@@ -1,3 +1,4 @@
+local nvim_lsp = require('lspconfig')
 local utils = require('lsp.utils')
 local common_on_attach = utils.common_on_attach
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -24,12 +25,12 @@ vim.api.nvim_set_hl(0, "@lsp.type.property", { link = "Identifier" })
 vim.api.nvim_set_hl(0, "@lsp.type.variable", { link = "Identifier" })
 
 -- Go configuration
-vim.lsp.config.gopls = {
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_markers = { "go.work", "go.mod", ".git" },
+nvim_lsp.gopls.setup({
   capabilities = capabilities,
   on_attach = common_on_attach,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = { "go.work", "go.mod", ".git" },
   settings = {
     gopls = {
       completeUnimported = true,
@@ -39,5 +40,5 @@ vim.lsp.config.gopls = {
       },
     },
   },
-}
+})
 

lua/lsp/ts.lua
@@ -1,36 +1,36 @@
+local nvim_lsp = require('lspconfig')
 local utils = require('lsp.utils')
 local common_on_attach = utils.common_on_attach
-
 
 -- add capabilities from nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- TypeScript/JavaScript via ts_ls (formerly tsserver)
-vim.lsp.config.ts_ls = {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-  root_markers = { "tsconfig.json", "package.json", ".git" },
+-- TypeScript/JavaScript via tsserver
+nvim_lsp.tsserver.setup({
   capabilities = capabilities,
   on_attach = function(client, bufnr)
+    -- Disable tsserver formatting in favor of prettier or null-ls
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
     common_on_attach(client, bufnr)
   end,
-}
+  root_dir = nvim_lsp.util.root_pattern("tsconfig.json", "package.json", ".git"),
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+  cmd = { "typescript-language-server", "--stdio" },
+})
 
 -- Optional: ESLint LSP for linting
-vim.lsp.config.eslint = {
-  cmd = vim.lsp.config.eslint.cmd,
-  filetypes = vim.lsp.config.eslint.filetypes,
-  root_markers = { ".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git" },
+nvim_lsp.eslint.setup({
   capabilities = capabilities,
   on_attach = function(client, bufnr)
+    -- You can enable auto-fix on save
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       command = "EslintFixAll"
     })
     common_on_attach(client, bufnr)
   end,
-}
+  root_dir = nvim_lsp.util.root_pattern(".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git"),
+})
 

lua/plugins/lspconfig.lua
@@ -1,3 +1,4 @@
+local nvim_lsp = require('lspconfig')
 local utils = require('lsp.utils')
 local common_on_attach = utils.common_on_attach
 
@@ -8,20 +9,14 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 -- Enable language servers with common settings
 local servers = {"bashls", "clangd", "pyright", "jsonls", "dockerls"}
 for _, lsp in ipairs(servers) do
-  vim.lsp.config[lsp] = {
-    cmd = vim.lsp.config[lsp].cmd,
-    filetypes = vim.lsp.config[lsp].filetypes,
-    root_markers = vim.lsp.config[lsp].root_markers,
+  nvim_lsp[lsp].setup({
     on_attach = common_on_attach,
     capabilities = capabilities,
-  }
+  })
 end
 
 
-vim.lsp.config["pyright"] = {
-  cmd = vim.lsp.config["pyright"].cmd,
-  filetypes = vim.lsp.config["pyright"].filetypes,
-  root_markers = vim.lsp.config["pyright"].root_markers,
+nvim_lsp["pyright"].setup({
   capabilities = capabilities,
   on_attach = common_on_attach,
   settings={
@@ -30,7 +25,7 @@ vim.lsp.config["pyright"] = {
       },
       pyright = {}
   }
-}
+})
 
 require('lsp.rust')
 -- require('lsp.sumneko')
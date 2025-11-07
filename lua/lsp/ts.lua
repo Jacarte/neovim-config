2fedca0dc136dfebbe041a3b98eebfb35dd94e5b
Javier Cabrera <xppcoder@gmail.com>
2025-11-07 13:52:09 +0100

    Fix: Restore lspconfig.setup() pattern to fix broken LSP navigation
    Commit 6b5259f broke LSP navigation by switching from require('lspconfig')
    with .setup() to vim.lsp.config declarations. The vim.lsp.config approach
    only registers configs but doesn't auto-start servers without additional
    autocmd logic.
    
    This fix reverts to the proven working pattern:
    - lua/lsp/golang.lua: Use nvim_lsp.gopls.setup()
    - lua/lsp/ts.lua: Use nvim_lsp.tsserver.setup() and nvim_lsp.eslint.setup()
    - lua/plugins/lspconfig.lua: Use nvim_lsp[server].setup() pattern
    
    LSP servers (gopls, ts_ls) now auto-start when opening matching file types.
    Verified with headless testing: both gopls and ts_ls clients connect properly.

--- lua/lsp/ts.lua
+++ lua/lsp/ts.lua
@@ -1,36 +1,36 @@
+local nvim_lsp = require('lspconfig')
 local utils = require('lsp.utils')
 local common_on_attach = utils.common_on_attach
-
 
 -- add capabilities from nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
--- TypeScript/JavaScript via ts_ls (formerly tsserver)
-vim.lsp.config.ts_ls = {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-  root_markers = { "tsconfig.json", "package.json", ".git" },
+-- TypeScript/JavaScript via tsserver
+nvim_lsp.tsserver.setup({
   capabilities = capabilities,
   on_attach = function(client, bufnr)
+    -- Disable tsserver formatting in favor of prettier or null-ls
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
     common_on_attach(client, bufnr)
   end,
-}
+  root_dir = nvim_lsp.util.root_pattern("tsconfig.json", "package.json", ".git"),
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+  cmd = { "typescript-language-server", "--stdio" },
+})
 
 -- Optional: ESLint LSP for linting
-vim.lsp.config.eslint = {
-  cmd = vim.lsp.config.eslint.cmd,
-  filetypes = vim.lsp.config.eslint.filetypes,
-  root_markers = { ".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git" },
+nvim_lsp.eslint.setup({
   capabilities = capabilities,
   on_attach = function(client, bufnr)
+    -- You can enable auto-fix on save
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       command = "EslintFixAll"
     })
     common_on_attach(client, bufnr)
   end,
-}
+  root_dir = nvim_lsp.util.root_pattern(".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git"),
+})
 
